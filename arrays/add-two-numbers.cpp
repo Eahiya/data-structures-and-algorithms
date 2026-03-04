63f2db90ca6635af89357ff8bfd58377488d9d07
Eahiya <eahiyaazad@gmail.com>
2026-03-04 20:41:58 +0530

    Solved a new problem in arrays

---
+++ arrays/add-two-numbers.cpp
@@ -0,0 +1,46 @@
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution{
+    public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2){
+        ListNode* head = new ListNode(0);
+        ListNode* current = head;
+        int carry = 0;
+
+        while(l1 != nullptr || l2 != nullptr || carry != 0){
+            int val1 = 0;
+            if(l1 != nullptr){
+                val1 = l1->val;
+            }
+            int val2 = 0;
+            if(l2 != nullptr){
+                val2 = l2->val;
+            }
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+
+            current->next  = new ListNode(sum % 10);
+            current = current->next;
+
+            if(l1 != nullptr){
+                l1 = l1->next;
+            }
+            if(l2 != nullptr){
+                l2 = l2->next;
+            }
+        }
+        ListNode* result = head->next;
+        delete head;
+        return result;
+    }
+};
