9377cc5d5df154874db3f3a7ff946a18c925d996
Eahiya <eahiyaazad@gmail.com>
2026-03-04 19:46:44 +0530

    Add comments to the two sum problem

--- arrays/to-sum-os.cpp
+++ arrays/to-sum-os.cpp
@@ -11,17 +11,24 @@ class Solution{
     vector<int> twoSum(vector<int> &arr, int target){
 
         unordered_map <int, int> mp;
+        
         int n = arr.size();
 
         for(int i=0;i<n;i++){
-            
-            int complement = target - arr[i];
 
+            // Calculate the complement of the current number
+            int complement = target - arr[i];
+            
+            // check if the complement exists in the map
             if(mp.find(complement) != mp.end()){
                 return {mp[complement], i};
             }
+
+            // remember the current number and its position for future checks
             mp[arr[i]] = i;
         }
+
+        // Return an empty vector if no solution is found
         return {};
     }
 };
